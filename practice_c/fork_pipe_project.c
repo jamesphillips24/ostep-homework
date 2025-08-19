35f5488753eb7702aa17d700b97fe09db504e518
James Phillips <jamesphillips052404@gmail.com>
2025-08-19 00:24:22 -0400

    Added int to string

--- practice_c/fork_pipe_project.c
+++ practice_c/fork_pipe_project.c
@@ -4,14 +4,68 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+typedef struct Stack{
+    struct Stack* below;
+    char c;
+    int n;
+} Stack;
+
+void print_stack(Stack* s){
+    while(s->below != NULL){
+        printf("%c -- %d\n", s->c, s->n);
+        s = s->below;
+    }
+    return;
+}
+
+void pop(Stack** s){
+    Stack* t = *s;
+    *s = (*s)->below;
+    free(t);
+}
+
+char* int_to_char(int num){
+    Stack* s = malloc(sizeof(Stack));
+    s->below = NULL;
+    s->n = 0;
+
+    int magnitude = 0;
+    while(num > 0){
+        char c = '0' + num%10;
+
+        Stack* t = malloc(sizeof(Stack));
+        t->below = s;
+        t->c = c;
+        t->n = s->n + 1;
+        s = t;
+
+        num /= 10;
+        magnitude++;
+    }
+
+    int length = s->n;
+    char* buffer = malloc(length + 1);
+    buffer[length] = '\0';
+
+    while(s->n > 0){
+        char c = s->c;
+        buffer[length - s->n] = c;
+        pop(&s);
+    }
+
+    free(s);
+
+    return buffer;
+}
+
 int main(int argc, char *argv[])
 {
+    char* buffer = int_to_char(756);
+    printf("%s\n", buffer);
+
+
     int pipe_arr[2];
     pipe(pipe_arr);
-
-    int one = 1;
-    printf("%c", ('0' + 1));
-
     int fork_num = fork();
 
     if(fork_num < 0){
